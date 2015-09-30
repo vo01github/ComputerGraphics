bdb04da200dc7963ae664ca0fb51fa0e2bb3ec1d
vo01github <hxt2014bbg@icloud.com>
2015-09-30 18:39:11 +0800

    2

---
+++ OpenGL_Study_Code/《BeginningOpenGLGameProgramming》/Classes/01-firstGraphics.h
@@ -0,0 +1,33 @@
+﻿
+#include<stdlib.h>
+#include<cstdio>
+#include<stdlib.h>
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+#include<gl/glut.h> 
+
+void myDisplay(void)  
+{  
+	glClear(GL_COLOR_BUFFER_BIT);     //清除颜色  
+	glRectf(-0.5f,-0.5f,0.5f,0.5f);    //画一个矩形  
+	glFlush();   //让前面的命令立即执行而不是在缓冲区，与fflush(stdout)作用类似  
+}  
+
+#if 0
+
+int _tmain(int argc,_TCHAR* argv[])  
+{  
+	glutInit(&argc,(char**)argv);    //初始化glut,必须调用，复制黏贴这句话即可  
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); //设置显示方式，RGB、单缓冲。当然还有GLUT_INDEX索引颜色 GLUT_DOUBLE双缓冲(Qt中看到过双缓冲)  
+	glutInitWindowPosition(100,100);   //位置  
+	glutInitWindowSize(400,400);//窗口大小  
+	glutCreateWindow("第一个OpenGL程序");  //创建窗口，设置标题  
+	glutDisplayFunc(&myDisplay);  // 当绘制窗口时调用myDisplay，像Cocos2d-x刷帧Draw中的操作  
+	glutMainLoop();  //消息循环  
+	return 0;  
+}  
+
+#endif
