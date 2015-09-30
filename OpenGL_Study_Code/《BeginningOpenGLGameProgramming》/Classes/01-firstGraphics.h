f43fd46cb40f55eb9fd6cfc944845146ab4d83eb
vo01github <hxt2014bbg@icloud.com>
2015-10-01 01:14:01 +0800

    1

--- OpenGL_Study_Code/《BeginningOpenGLGameProgramming》/Classes/01-firstGraphics.h
+++ OpenGL_Study_Code/《BeginningOpenGLGameProgramming》/Classes/01-firstGraphics.h
@@ -4,30 +4,69 @@
 #include<stdlib.h>
 #include <cstdio>
 #include <iostream>
-
 using namespace std;
 
 #include<gl/glut.h> 
 
-void myDisplay(void)  
+// 一个通用 opengl 框架
+/*
+	http://blog.csdn.net/laomai/article/details/7269511
+*/
+
+// 画矩形
+void myDisplay_Rect(void)  
 {  
-	glClear(GL_COLOR_BUFFER_BIT);     //清除颜色  
-	glRectf(-0.5f,-0.5f,0.5f,0.5f);    //画一个矩形  
-	glFlush();   //让前面的命令立即执行而不是在缓冲区，与fflush(stdout)作用类似  
+	glClear(GL_COLOR_BUFFER_BIT);		//清除颜色  
+	glRectf(-0.5f,-0.5f,0.5f,0.5f);		//画一个矩形  
+	glFlush();							//让前面的命令立即执行而不是在缓冲区，与fflush(stdout)作用类似  
 }  
 
-#if 0
-
-int _tmain(int argc,_TCHAR* argv[])  
+// 画点坐标
+void myDisplay_POINTS(void)  
 {  
-	glutInit(&argc,(char**)argv);    //初始化glut,必须调用，复制黏贴这句话即可  
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); //设置显示方式，RGB、单缓冲。当然还有GLUT_INDEX索引颜色 GLUT_DOUBLE双缓冲(Qt中看到过双缓冲)  
-	glutInitWindowPosition(100,100);   //位置  
-	glutInitWindowSize(400,400);//窗口大小  
-	glutCreateWindow("第一个OpenGL程序");  //创建窗口，设置标题  
-	glutDisplayFunc(&myDisplay);  // 当绘制窗口时调用myDisplay，像Cocos2d-x刷帧Draw中的操作  
-	glutMainLoop();  //消息循环  
+	glClear(GL_COLOR_BUFFER_BIT);  
+	glBegin(GL_POINTS);  
+	glVertex2f(-0.5,-0.5);		//	点P1  
+	glVertex2f(-0.5,0.5);		//	点P2  
+	glVertex2f(0.5,0.5);		//	点P3  
+	glVertex2f(0.5,-0.5);		//	点P4  
+	glEnd();  
+	glFlush(); 
+}  
+
+// 线段的绘制
+void myDisplay_LINES(void)  
+{  
+	glClear(GL_COLOR_BUFFER_BIT);  
+	glBegin(GL_LINES);
+
+	glVertex2f(-0.5,-0.5);		//	点P1  
+	glVertex2f(-0.5,0.5);		//	点P2 
+
+	glVertex2f(0.5,0.5);		//	点P3  
+	glVertex2f(0.5,-0.5);		//	点P4  
+
+	glVertex2f(-0.5,0.5);		//	点P2  
+	glVertex2f(0.5,0.5);		//	点P3 
+
+	glVertex2f(-0.5,-0.5);		//	点P1
+	glVertex2f(0.5,-0.5);		//	点P4  
+
+	glEnd();  
+	glFlush(); 
+}  
+
+
+
+int firstGraphics(int argc,_TCHAR* argv[])  
+{  
+	glutInit(&argc,(char**)argv);					// 初始化glut,必须调用，复制黏贴这句话即可  
+	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);	// 设置显示方式，RGB、单缓冲。当然还有GLUT_INDEX索引颜色 GLUT_DOUBLE双缓冲(Qt中看到过双缓冲)  
+	glutInitWindowPosition(100,100);				// 位置  
+	glutInitWindowSize(400,400);					// 窗口大小  
+	glutCreateWindow("第一个OpenGL程序");			// 创建窗口，设置标题  
+	glutDisplayFunc(&myDisplay_LINES);					// 当绘制窗口时调用myDisplay，像Cocos2d-x刷帧Draw中的操作  
+	glutMainLoop();									// 消息循环  
 	return 0;  
 }  
 
-#endif
--- OpenGL_Study_Code/《BeginningOpenGLGameProgramming》/proj.win32/main.cpp
+++ OpenGL_Study_Code/《BeginningOpenGLGameProgramming》/proj.win32/main.cpp
@@ -1,27 +1,13 @@
 // 《BeginningOpenGLGameProgramming》.cpp : 定义控制台应用程序的入口点。
 #include "stdafx.h"
 
-//#include "01-firstGraphics.h"
+#include "01-firstGraphics.h"
 
 
-void myDisplay(void)  
-{  
-	glClear(GL_COLOR_BUFFER_BIT);     //清除颜色  
-	glRectf(-0.5f,-0.5f,0.5f,0.5f);    //画一个矩形  
-	glFlush();   //让前面的命令立即执行而不是在缓冲区，与fflush(stdout)作用类似  
-}  
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-
-	glutInit(&argc,(char**)argv);    //初始化glut,必须调用，复制黏贴这句话即可  
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE); //设置显示方式，RGB、单缓冲。当然还有GLUT_INDEX索引颜色 GLUT_DOUBLE双缓冲(Qt中看到过双缓冲)  
-	glutInitWindowPosition(100,100);   //位置  
-	glutInitWindowSize(400,400);//窗口大小  
-	glutCreateWindow("第一个OpenGL程序");  //创建窗口，设置标题  
-	glutDisplayFunc(&myDisplay);  // 当绘制窗口时调用myDisplay，像Cocos2d-x刷帧Draw中的操作  
-	glutMainLoop();  //消息循环 
+	firstGraphics(argc, argv);
 
 	return 0;
 }
-
